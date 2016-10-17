70e29b0c6675cee55bc2357e9f2a684a2773cda7
mej71 <mej7171@gmail.com>
2016-10-17 13:54:02 -0500

    MenuController base
    Pause Menu now controlled by a base class called MenuController.  Will
    use this as a base class for other menus

---
+++ Assets/Resources/Scripts/Utilities/MenuController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+//Base class for choice boxes
+public class ChoiceController : MonoBehaviour {
+
+    protected Image imageBox, imageArrow;
+    protected GameObject textObject;
+    protected int numMenuItems;
+    protected int arrowPos;
+    protected string arrowLocation, imageBoxName, imageArrowName, textObjectName;
+    protected List<string> menuOptions = new List<string>();
+
+    //values for arrow movement
+    protected int arrow1024X, arrow1024Y, arrow1024Spacing;
+    protected int arrowOtherX, arrowOtherY, arrowOtherSpacing;
+    //values for object positioning
+    protected int imageBox1024Width, imageBox1024X, imageBox1024Y;
+    protected int imageArrow1024Width, imageArrow1024Height, imageArrow1024X, imageArrow1024Y;
+    protected int textObject1024Width, textObject1024Height, textObject1024X, textObject1024Y;
+    protected int imageBoxOtherWidth, imageBoxOtherX, imageBoxOtherY;
+    protected int imageArrowOtherWidth, imageArrowOtherHeight, imageArrowOtherX, imageArrowOtherY;
+    protected int textObjectOtherWidth, textObjectOtherHeight, textObjectOtherX, textObjectOtherY;
+
+    // Use this for initialization
+    protected void Start() {
+        numMenuItems = 0;
+        arrowPos = 0;
+        Sprite[] arrowSprites = Resources.LoadAll<Sprite>(arrowLocation);
+        Image[] tempImages = GetComponentsInChildren<Image>(true);
+        foreach (Image image in tempImages) {
+            if (image.name.Equals(imageBoxName)) {
+                imageBox = image;
+                imageBox.enabled = true;
+            } else if (image.name.Equals(imageArrowName)) {
+                image.GetComponent<UnityEngine.UI.Image>().sprite = arrowSprites[1];
+                imageArrow = image;
+                imageArrow.enabled = true;
+            }
+        }
+        foreach (Transform child in transform) {
+            if (child.name.Equals(textObjectName)) {
+                textObject = child.gameObject;
+                break;
+            }
+        }
+        positionChoiceBoxElements();
+    }
+
+    // Update is called once per frame
+    public void Update() {
+        bool arrowPosChanged = false;
+        if (Input.GetKeyUp(KeyCode.Alpha0)) {
+            imageBox.enabled = false;
+            imageArrow.enabled = false;
+            textObject.GetComponent<Text>().enabled = false;
+            Destroy(this);
+        } else if (Input.GetKeyDown(KeyCode.G)) { //Down
+            ++arrowPos;
+            if (arrowPos == menuOptions.Count) {
+                arrowPos = 0;
+            }
+            arrowPosChanged = true;
+        } else if (Input.GetKeyDown(KeyCode.H)) { //Up
+            --arrowPos;
+            if (arrowPos < 0) {
+                arrowPos = menuOptions.Count - 1;
+            }
+            arrowPosChanged = true;
+        }
+        if (arrowPosChanged) {
+            if (Screen.width == 1024) {
+                imageArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(arrow1024X, arrow1024Y - (arrowPos * arrow1024Spacing), 0);
+            } else {
+                imageArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(arrowOtherX, arrowOtherY - (arrowPos * arrowOtherSpacing), 0);
+            }
+        }
+    }
+
+    protected void positionChoiceBoxElements() {
+        if (Screen.width == 1024) {
+            imageBox.GetComponent<RectTransform>().sizeDelta = new Vector2(imageBox1024Width, menuOptions.Count*60);
+            imageBox.GetComponent<RectTransform>().anchoredPosition = new Vector3(imageBox1024X, imageBox1024Y, 0);
+            imageArrow.GetComponent<RectTransform>().sizeDelta = new Vector2(imageArrow1024Width, imageArrow1024Height);
+            imageArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(imageArrow1024X, imageArrow1024Y, 0);
+            textObject.GetComponent<RectTransform>().sizeDelta = new Vector2(textObject1024Width, textObject1024Height);
+            textObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(textObject1024X, textObject1024Y, 0);
+        } else {
+            imageBox.GetComponent<RectTransform>().sizeDelta = new Vector2(imageBoxOtherWidth, menuOptions.Count*60);
+            imageBox.GetComponent<RectTransform>().anchoredPosition = new Vector3(imageBoxOtherX, imageBoxOtherY, 0);
+            imageArrow.GetComponent<RectTransform>().sizeDelta = new Vector2(imageArrowOtherWidth, imageArrowOtherHeight);
+            imageArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(imageArrowOtherX, imageArrowOtherY, 0);
+            textObject.GetComponent<RectTransform>().sizeDelta = new Vector2(textObjectOtherWidth, textObjectOtherHeight);
+            textObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(textObjectOtherX, textObjectOtherY, 0);
+        }
+        //Fill menu text
+        textObject.GetComponent<Text>().enabled = false;
+        textObject.GetComponent<Text>().text = "";
+        for (int i = 0; i<menuOptions.Count-1; ++i ) {
+            textObject.GetComponent<Text>().text += menuOptions[i] + "\n";
+        }
+        textObject.GetComponent<Text>().text += menuOptions[menuOptions.Count-1];
+        textObject.GetComponent<Text>().enabled = true;
+    }
+
+}
--- Assets/Resources/Scripts/Utilities/PauseMenuController.cs
+++ Assets/Resources/Scripts/Utilities/PauseMenuController.cs
@@ -2,95 +2,48 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class PauseMenuController : MonoBehaviour {
-
-    private Image pauseBox;
-    private Image pauseArrow;
-    private GameObject pauseText;
-    private int numMenuItems;
-    private int arrowPos;
-
+public class PauseMenuController : ChoiceController {
 
     void Start() {
-        numMenuItems = 0;
-        arrowPos = 0;
-        Sprite[] arrowSprites = Resources.LoadAll<Sprite>("Graphics/Pictures/arrows");
-        Image[] tempImages = GetComponentsInChildren<Image>(true);
-        foreach (Image image in tempImages) {
-            if (image.name.Equals("Pause Skin")) {
-                pauseBox = image;
-                pauseBox.enabled = true;
-            } else if (image.name.Equals("Pause Selector")) {
-                image.GetComponent<UnityEngine.UI.Image>().sprite = arrowSprites[1];
-                pauseArrow = image;
-                pauseArrow.enabled = true;
-            }
-        }
-        foreach (Transform child in transform) {
-            if (child.name.Equals("Pause Text")) {
-                pauseText = child.gameObject;
-                break;
-            }
-        }
-        positionPauseMenu();
-        fillPauseText();
+        //Set desired objects/sprites to use (already created, and children of the game object this is attatched to)
+        arrowLocation = "Graphics/Pictures/arrows";
+        imageBoxName = "Pause Skin";
+        imageArrowName = "Pause Selector";
+        textObjectName = "Pause Text";
+        //Set desired positioning
+        arrow1024X = -275;
+        arrow1024Y = 22;
+        arrow1024Spacing = arrowOtherSpacing = 52;
+        arrowOtherX = -292;
+        arrowOtherY = 42;
+        imageBox1024Width = imageBoxOtherWidth = 290;
+        imageBox1024X = -15;
+        imageBox1024Y = 50;
+        imageBoxOtherX = -35;
+        imageBoxOtherY = 70;
+        imageArrow1024Width = imageArrow1024Height = 16;
+        imageArrow1024Height = imageArrowOtherHeight = 32;
+        imageArrow1024X = -275;
+        imageArrow1024Y = 22;
+        imageArrowOtherX = -292;
+        imageArrowOtherY = 42;
+        textObject1024Width = textObjectOtherWidth = 120;
+        textObject1024Height = textObjectOtherHeight = 280;
+        textObject1024X = -140;
+        textObject1024Y = 25;
+        textObjectOtherX = -155;
+        textObjectOtherY = 45;
+        updateMenuText();
+
+        base.Start();     
     }
 
-    void Update() {
-        if (Input.GetKeyUp(KeyCode.Space)) {
-            pauseBox.enabled = false;
-            pauseArrow.enabled = false;
-            pauseText.GetComponent<Text>().enabled = false;
-            Destroy(this);
-        } else if (Input.GetKeyDown(KeyCode.G)) { //Down
-            ++arrowPos;
-            if (arrowPos==numMenuItems) {
-                arrowPos = 0;
-            }
-            if (Screen.width == 1024) {
-                pauseArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(-275, 22 - arrowPos * 52, 0);
-            } else {
-                pauseArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(-292, 42 - arrowPos * 52, 0);
-            }
-        } else if (Input.GetKeyDown(KeyCode.H)) { //Up
-            --arrowPos;
-            if (arrowPos <0) {
-                arrowPos = numMenuItems-1;
-            }
-            if (Screen.width == 1024) {
-                pauseArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(-275, 22 - arrowPos * 52, 0);
-            } else {
-                pauseArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(-292, 42 - arrowPos * 52, 0);
-            }
-        }
-    }
-
-    void positionPauseMenu() {
-        if (Screen.width == 1024) {
-            pauseBox.GetComponent<RectTransform>().sizeDelta = new Vector2(290, 300);
-            pauseBox.GetComponent<RectTransform>().anchoredPosition = new Vector3(-15, 50, 0);
-            pauseText.GetComponent<RectTransform>().sizeDelta = new Vector2(120, 280);
-            pauseText.GetComponent<RectTransform>().anchoredPosition = new Vector3(-140, 25, 0);
-            pauseArrow.GetComponent<RectTransform>().sizeDelta = new Vector2(16, 32);
-            pauseArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(-275, 22, 0);
-        } else {
-            pauseBox.GetComponent<RectTransform>().sizeDelta = new Vector2(290, 300);
-            pauseBox.GetComponent<RectTransform>().anchoredPosition = new Vector3(-35, 70, 0);
-            pauseText.GetComponent<RectTransform>().sizeDelta = new Vector2(120, 280);
-            pauseText.GetComponent<RectTransform>().anchoredPosition = new Vector3(-155, 45, 0);
-            pauseArrow.GetComponent<RectTransform>().sizeDelta = new Vector2(16, 32);
-            pauseArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(-292, 42, 0);
-        }
-    }
-
-    void fillPauseText() {
-        pauseText.GetComponent<Text>().text = "";
+    void updateMenuText() {
+        menuOptions.Clear();
         //Add each menu item
-        pauseText.GetComponent<Text>().text += "Pokédex";
-        pauseText.GetComponent<Text>().text += "\nPokémon";
-        pauseText.GetComponent<Text>().text += "\nBag";
-        numMenuItems = 3;
-        pauseText.GetComponent<Text>().enabled = true;
+        menuOptions.Add("Pokédex");
+        menuOptions.Add("Pokémon");
+        menuOptions.Add("Bag");
     }
 
 }
